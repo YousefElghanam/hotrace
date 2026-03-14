0a1a1757ad291d7d37432ee55f6c7d1bb9e549e1
Autcoder <150786670+Autcoder@users.noreply.github.com>
2026-03-14 19:22:08 +0100

    fix manual mode not working properly

--- main.c
+++ main.c
@@ -6,79 +6,88 @@
 /*   By: flenski <flenski@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 11:59:38 by flenski           #+#    #+#             */
-/*   Updated: 2026/03/14 18:49:09 by flenski          ###   ########.fr       */
+/*   Updated: 2026/03/14 19:21:09 by flenski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hotrace.h"
 #include <unistd.h>
 
-static char	*exec_search(char *buf, t_HashMap *hmap)
+static char	*exec_search(char *buf, t_HashMap *hmap, size_t *rem_len)
 {
 	char	*val;
-	char	*next;
+	char	*n;
 
 	while (buf && *buf)
 	{
-		next = buf;
-		while (*next && *next != '\n')
-			next++;
-		if (*next == '\n')
+		n = buf;
+		while (*n && *n != '\n')
+			n++;
+		if (*n == '\n')
 		{
-			*next = '\0';
+			*n = '\0';
 			val = (char *)hashmap_get(hmap, buf);
 			if (val)
 				buffered_out(val, 0);
 			else
 				buffered_out("Not found", 0);
-			buf = next + 1;
+			buf = n + 1;
 		}
 		else
 			break ;
 	}
+	*rem_len = 0;
+	if (buf && *buf)
+		while (buf[*rem_len])
+			(*rem_len)++;
 	return (buf);
 }
 
-static void	search_loop(char *leftover, t_HashMap *hmap)
+static void	search_loop(char *rem, t_HashMap *hmap)
 {
-	char	*rem;
+	char	buf[16384];
 	ssize_t	ret;
 	size_t	len;
-	char	buf[16384];
 
-	rem = exec_search(leftover, hmap);
-	len = ft_strlen(rem);
+	rem = exec_search(rem, hmap, &len);
 	ft_memmove(buf, rem, len);
+	buffered_out(NULL, 1);
 	while (1)
 	{
 		ret = read(0, buf + len, 16383 - len);
 		if (ret <= 0)
 			break ;
 		buf[len + ret] = '\0';
-		rem = exec_search(buf, hmap);
-		len = ft_strlen(rem);
+		rem = exec_search(buf, hmap, &len);
 		ft_memmove(buf, rem, len);
+		buffered_out(NULL, 1);
 	}
-	buffered_out(NULL, 1);
 }
 
-static void	get_search_ptr(char **buf, char **search_ptr)
+static char	*get_db(char *buf)
 {
 	ssize_t	ret;
-	int		total;
+	int		tot;
+	int		i;
 
-	total = 0;
-	*search_ptr = NULL;
+	tot = 0;
 	while (1)
 	{
-		ret = read(0, *buf + total, 1024 * 1024);
+		ret = read(0, buf + tot, 1048576);
 		if (ret <= 0)
-			break ;
-		total += ret;
-		(*buf)[total] = '\0';
-		*search_ptr = ft_strstr(*buf, "\n\n");
-		if (*search_ptr)
-			break ;
+			return (NULL);
+		i = (tot > 0) ? tot - 1 : 0;
+		tot += ret;
+		buf[tot] = '\0';
+		while (i < tot - 1)
+		{
+			if (buf[i] == '\n' && buf[i + 1] == '\n')
+			{
+				buf[i] = '\0';
+				return (buf + i + 2);
+			}
+			i++;
+		}
 	}
 }
 
@@ -88,15 +97,12 @@ int	main(void)
 	char		*search_ptr;
 	t_HashMap	*hmap;
 
-	search_ptr = NULL;
-	buf = malloc(1024 * 1024 * 1024);
+	buf = malloc(1024 * 1024 * 512);
 	if (!buf)
 		return (1);
-	get_search_ptr(&buf, &search_ptr);
+	search_ptr = get_db(buf);
 	if (!search_ptr)
 		return (free(buf), 0);
-	*search_ptr = '\0';
-	search_ptr += 2;
 	hmap = hashmap_create(count_entries(buf) * 2);
 	fill_map(buf, hmap);
 	search_loop(search_ptr, hmap);
